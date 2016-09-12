f6c9dc9db95d22e0c4894540cc3145f139a7513b
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-09-12 21:03:14 +0100

    Remove breadcrumb

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -138,7 +138,6 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
